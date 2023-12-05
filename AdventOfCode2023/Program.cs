8258e0399fc3c5e3747f306ca1827fbd8982accd
m.tre <m.tre@skbkontur.ru>
2023-12-05 13:16:26 +0500

    start day 5

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2023.Solutions;
 
-var solution = new Day4();
+var solution = new Day5();
 solution.Solution();
---
+++ AdventOfCode2023/Solutions/Day5.cs
@@ -0,0 +1,83 @@
+﻿namespace AdventOfCode2023.Solutions;
+
+public class Day5
+{
+    private readonly Dictionary<long, long> _seedToSoil = new();
+    private readonly Dictionary<long, long> _soilToFertilizer = new();
+    private readonly Dictionary<long, long> _fertilizerToWater = new();
+    private readonly Dictionary<long, long> _waterToLight = new();
+    private readonly Dictionary<long, long> _lightToTemperature = new();
+    private readonly Dictionary<long, long> _temperatureToHumidity = new();
+    private readonly Dictionary<long, long> _humidityToLocation = new();
+
+    public void Solution()
+    {
+        var input = File.ReadAllText($"./Inputs/{GetType().Name.ToLowerInvariant()}.txt")
+            .Split("\n")
+            .Select(x => x.Trim())
+            .ToList();
+
+        // seeds: 79 14 55 13
+        var seeds = input
+            .Take(1)
+            .Single()
+            .Split(':', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)[1]
+            .Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToList();
+
+        var currentDict = _seedToSoil;
+
+        foreach (var line in input.Skip(1))
+        {
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            if (line.Contains("map"))
+            {
+                var mapName = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)[0];
+                currentDict = mapName switch
+                {
+                    "seed-to-soil" => _seedToSoil,
+                    "soil-to-fertilizer" => _soilToFertilizer,
+                    "fertilizer-to-water" => _fertilizerToWater,
+                    "water-to-light" => _waterToLight,
+                    "light-to-temperature" => _lightToTemperature,
+                    "temperature-to-humidity" => _temperatureToHumidity,
+                    "humidity-to-location" => _humidityToLocation,
+                    _ => throw new Exception("Unknown map")
+                };
+
+                continue;
+            }
+            
+            var coords = line.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+            var destRangeStart = long.Parse(coords[0]);
+            var sourceRangeStart = long.Parse(coords[1]);
+            var lengthRange = long.Parse(coords[2]);
+            
+            for (var i = 0; i < lengthRange; i++)
+                currentDict[sourceRangeStart + i] = destRangeStart + i;
+        }
+
+
+        Console.WriteLine(Part1());
+        Console.WriteLine(Part2());
+    }
+
+    private string Part1(List<long> seeds)
+    {
+        var res = "";
+        
+        
+        
+        return $"Part 1: {res}";
+    }
+
+    private string Part2()
+    {
+        var res = "";
+
+        return $"Part 2: \n{res}";
+    }
+}
